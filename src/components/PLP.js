8c78e51cae856c7e2f43a3cc6b983342eff202a9
nass <ngalmikhlafi@gmail.com>
2022-10-06 10:59:10 +0400

    loading screen for initial render

--- src/components/PLP.js
+++ src/components/PLP.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { client } from "../index";
 import { CATEGORIES } from "../GraphQL/Queries";
 
+
 export default class PLP extends Component {
   state = {
     categories: [],
@@ -20,11 +21,19 @@ export default class PLP extends Component {
   }
 
   render() {
+    if (this.state.categories.length === 0) {
+      return <div>Loading...</div>;
+    }
     const { categories } = this.state;
     console.log(categories);
     return (
       <div>
         <h1>Categories</h1>
+        <ul>
+          {categories.map((category, i) => (
+            <li key={i}>{category.name}</li>
+          ))}
+        </ul>
         
        
       </div>
