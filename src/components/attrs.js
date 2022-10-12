3556156f9171bccb81a74be4232a9f718450c806
nass <ngalmikhlafi@gmail.com>
2022-10-12 19:36:30 +0400

    layout attrs

--- src/components/attrs.js
+++ src/components/attrs.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 class Attrs extends Component {
   render() {
@@ -11,20 +11,29 @@ class Attrs extends Component {
             <h1>{this.props.attrs.name}</h1>
           </div>
           <div>
-            {
-              this.props.attrs.attributes.map((attr, i) => (
-                <div key={attr.id}>
-                  <h1>{attr.name}:</h1>
-                  {
-                    attr.items.map((item, i) => (
-                      <div key={item.id}>
-                        <h1>{item.displayValue}</h1>
-                      </div>
-                    ))
-                  }
+            {this.props.attrs.attributes.map((attr, i) => (
+              <div key={attr.id}>
+                <h1>{attr.name}:</h1>
+                <div className="itemAtrrs">
+                  {attr.items.map((item, i) => (
+                    <div key={item.id}>
+                      <h1>{item.displayValue}</h1>
+                    </div>
+                  ))}
                 </div>
-              ))
-            }
+              </div>
+            ))}
+            <div>
+              <h1>{this.props.attrs.prices[0].__typename}:</h1>
+              <h1>
+                {this.props.attrs.prices[0].currency.symbol}{" "}
+                {this.props.attrs.prices[0].amount}
+              </h1>
+            </div>
+            <div>
+              <button>add to cart</button>
+            </div>
+            <div>{this.props.attrs.description}</div>
           </div>
         </div>
       </div>
