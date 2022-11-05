332763ada197efdda8aaddb22a8ffb62a655bc5a
nass <ngalmikhlafi@gmail.com>
2022-11-05 15:54:10 +0400

    adding items to cart works

--- src/App.js
+++ src/App.js
@@ -47,11 +47,6 @@ class App extends Component {
     this.setState({ pdpItem: item, location: location });
   };
 
-  
-
-
-  
-
   addToCart = (item) => {
     console.log(item);
     // check if item is already in cart
@@ -62,68 +57,17 @@ class App extends Component {
     // add item to cart if it is not already in cart
     if (itemInCart === undefined) {
       this.setState({ cart: [...this.state.cart, item] });
-
-
     } else {
       //if item is already in cart, increment quantity
       let newCart = this.state.cart.map((cartItem) => {
         if (cartItem.itemInfo.id === item.itemInfo.id) {
-          cartItem.quantity += item.quantity;
+          cartItem.attributes = item.attributes;
         }
         return cartItem;
-      }
-      );
+      });
+
       this.setState({ cart: newCart });
     }
-       //   console.log(itemInCart);
-    //   let valsOfItem = [];
-    //   let valsOfItemInCart = [];
-
-    //   item.attributes.forEach((attr) => {
-    //     valsOfItem.push(attr.value);
-    //   });
-
-    //   itemInCart.attributes.forEach((attr) => {
-    //     valsOfItemInCart.push(attr.value);
-    //   });
-    
-
-
-    // this.setState({ cart: [...this.state.cart, item] });
-   
-    // console.log(itemInCart);
-   
-    // if (itemInCart === undefined) {
-    //   // add item to cart
-    //   console.log("item not in cart");
-    //   this.setState({ cart: [...this.state.cart, item] });
-    // } else {
-    //   console.log(itemInCart);
-     {/* let valsOfItem = [];
-      let valsOfItemInCart = [];
-
-      item.attributes.forEach((attr) => {
-        valsOfItem.push(attr.value);
-      });
-
-      itemInCart.attributes.forEach((attr) => {
-        valsOfItemInCart.push(attr.value);
-      });
-
-    //   console.log(valsOfItem);
-    //   console.log(valsOfItemInCart);
-
-      if (JSON.stringify(valsOfItem) === JSON.stringify(valsOfItemInCart)) {
-        // increment quantity of item in cart
-        let newCart = this.state.cart.map((cartItem) => {
-          if (cartItem.itemInfo.id === item.itemInfo.id) {
-            cartItem.quantity += 1;
-          }
-          return cartItem;
-        });
-        this.setState({ cart: newCart });
-      }
-    }*/}
   };
 
   toggleCartOverlay = () => {
